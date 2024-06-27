0e800e4438554e05bdc6393322cf1ad553d784ca
GodlyHamster <wouter.van.dompselaar@gmail.com>
2024-06-27 10:40:03 +0200

    Added rotate attack function to attackscriptableobject

--- Assets/Scripts/Attacks/PlayerAttack.cs
+++ Assets/Scripts/Attacks/PlayerAttack.cs
@@ -22,39 +22,14 @@ public class PlayerAttack : MonoBehaviour, ITurnComponent
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            AttackScriptableObject currentAttack = Instantiate(_attack);
-            AttackRotation atr = MouseDir();
-            if (atr == AttackRotation.NONE) return;
-
-            for (int i = 0; i < _attack.positions.Length; i++)
-            {
-                currentAttack.positions[i] = currentAttack.positions[i].Rotate((float)atr);
-            }
             Vector2Int currentPos = DungeonGrid.instance.GetPos(gameObject);
+            AttackScriptableObject currentAttack = Instantiate(_attack);
+
+            Vector2 characterScreenPos = Camera.main.WorldToScreenPoint(new Vector3(currentPos.x, currentPos.y));
+
+            currentAttack.AttackTowards(characterScreenPos, Input.mousePosition);
             AttackManager.instance.AddAttackAtPos(currentAttack, currentPos);
             TurnCompleteInvoker.Invoke();
-        }
-    }
-
-    public AttackRotation MouseDir()
-    {
-        Vector2 mouse = Input.mousePosition;
-        Vector2 character = Camera.main.WorldToScreenPoint(transform.position);
-        Vector2 normal = (mouse - character).normalized;
-        Vector2Int normalInt = new Vector2Int(Mathf.RoundToInt(normal.x), Mathf.RoundToInt(normal.y));
-        switch (normalInt)
-        {
-            case Vector2Int v when v.Equals(Vector2Int.right):
-                return AttackRotation.RIGHT;
-            case Vector2Int v when v.Equals(Vector2Int.up):
-                return AttackRotation.UP;
-            case Vector2Int v when v.Equals(Vector2Int.left):
-                return AttackRotation.LEFT;
-            case Vector2Int v when v.Equals(Vector2Int.down):
-                return AttackRotation.DOWN;
-            default:
-                return AttackRotation.NONE;
-
         }
     }
 }
